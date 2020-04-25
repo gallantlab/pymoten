d1c43518760a61111c5bc130361d20b0244d5325
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2020-04-26 00:54:57 +0100

    DOC: wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['numpydoc',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
