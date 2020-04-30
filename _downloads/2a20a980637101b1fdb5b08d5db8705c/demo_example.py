e3d7fe91639d596fd3a76bf570fc6b6c12468ace
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 01:26:04 +0000

    Deploy gallantlab/pymoten to github.com/gallantlab/pymoten.git:gh-pages

--- _downloads/2a20a980637101b1fdb5b08d5db8705c/demo_example.py
+++ _downloads/2a20a980637101b1fdb5b08d5db8705c/demo_example.py
@@ -16,7 +16,7 @@ video_file = 'http://anwarnunez.github.io/downloads/avsnr150s24fps_tiny.mp4'
 # .. raw:: html
 #
 #     <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto;">
-#         <iframe src="http://anwarnunez.github.io/downloads/avsnr150s24fps_tiny.mp4" frameborder="0" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+#         <iframe src="https://anwarnunez.github.io/downloads/avsnr150s24fps_tiny.mp4" frameborder="0" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
 #     </div>
 
 
